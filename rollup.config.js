08b4e650ee6a405b7c8d2dd32f0286314296166c
Bryan <54081465+arthvadrr@users.noreply.github.com>
2025-06-22 01:58:26 -0500

    Working insert

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,17 @@
-import { string } from 'rollup-plugin-string';
+import replace from '@rollup/plugin-replace';
+import dotenv from 'dotenv';
 import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
 import terser from '@rollup/plugin-terser';
 import sass from 'rollup-plugin-sass';
+import { string } from 'rollup-plugin-string';
+
+dotenv.config();
 
 export default [
 	{
 		input: './src/js/main.js',
+		context: 'window',
 		output: {
 			file: './public/build/main.min.js',
 			format: 'iife',
@@ -13,12 +19,18 @@ export default [
 		},
 		plugins: [
 			string({ include: '**/*.html' }),
-			resolve(),
+			resolve({ browser: true, preferBuiltins: false }),
+			commonjs(),
 			terser(),
 			sass({
 				api: 'modern',
 				output: './public/build/main.min.css',
 				options: { style: 'compressed' },
+			}),
+			replace({
+				preventAssignment: true,
+				'process.env.SERVER_URI': JSON.stringify(process.env.SERVER_URI),
+				'process.env.ANON_KEY': JSON.stringify(process.env.ANON_KEY),
 			}),
 		],
 	},
